1f80d8ce25b3632f0a1e6ad6b3aadfdc6b694a12
Promipi <isabellamorinigo@gmail.com>
2021-03-08 18:07:12 -0300

    Chat Estable

---
+++ Chat.Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace Chat.Client
+{
+    class Program
+    {
+        static HubConnection connection;
+        static async Task Main(string[] args)
+        {
+            connection = new HubConnectionBuilder()
+                             .WithUrl("https://localhost:5001/pruebaChatHub")
+                             .Build();
+
+            await connection.StartAsync(); //establecemmos la conexion
+
+
+            connection.On<string>("AwaitMessage", (message) =>
+            {
+                Console.WriteLine(message);
+            });
+
+            while(true)
+            {
+                var message = Console.ReadLine();
+                await connection.SendAsync("SendMessage", message);
+            }
+        }
+    }
+}
---
+++ Chat.Server/ChatPruebaHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.Server
+{
+    public class ChatPruebaHub : Hub
+    {
+        public override Task OnConnectedAsync() //cuiando se conecta un usuario
+        {
+            Console.WriteLine($"Usuario Conectado {Context.ConnectionId} ");
+            return Task.CompletedTask;
+        }
+
+        [HubMethodName("SendMessage") ]
+        public async Task SendMessage(string message) //para enviar un mensaje
+        {
+            await Clients.Others.SendAsync("AwaitMessage", message);
+        }
+    }
+}
